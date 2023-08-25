3d2697ee73dc4c7ea63d91a3add89da9e85a6c03
glitton <glcodeworks@gmail.com>
2023-08-25 17:31:12 -0500

    completed and added reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,51 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
+import data from "./data";
 
 const App = () => {
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
-  return <h2>Lorem Ipsum Starter</h2>;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setCount(1);
+    setText([]);
+  };
+  return (
+    <section className='section-center'>
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          min='1'
+          step='1'
+          max='8'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn' type='submit'>
+          generate
+        </button>
+        <button className='btn btn-reset' onClick={handleClick}>
+          reset
+        </button>
+      </form>
+
+      <article className='lorem-text'>
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
